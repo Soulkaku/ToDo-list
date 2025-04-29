f2697937b0e1815213853e44ac252749ab2bf696
Soulkaku <tangogitlab@gmail.com>
2025-04-29 16:55:31 -0300

    funcionando como deveria

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ import express from "express";
 import http from "http";
 import path from "path";
 import { fileURLToPath } from "url";
-import { server } from "socket.io";
+import { Server } from "socket.io";
 import exphbs from "express-handlebars";
 
 
 const app = express();
 
 const httpServer = http.createServer(app);
-const io = new server(httpServer);
+const io = new Server(httpServer);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -24,3 +24,24 @@ app.engine("hbs", exphbs.engine({
 app.set("views", path.join(`${__dirname}/SRC/Views`));
 app.use(express.static(path.join(`${__dirname}/Public`)));
 app.use(express.urlencoded( {extended : true} ));
+
+
+app.get("/", (req, res) => {
+    res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login", { layout : "main"});
+});
+
+app.get("/ToDo-list", (req, res) => {
+    res.render("taskList", { layout : "main" });
+});
+
+const PORT = 3000;
+
+httpServer.listen(PORT, () => {
+    console.log(`Listening in Port ${PORT} http://localhost:${PORT}/`);
+});
+
+export default io;
