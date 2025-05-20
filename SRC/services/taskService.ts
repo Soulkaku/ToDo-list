9eece8ddf44d00c847438d7a2f88e4f2df27d6f1
Soulkaku <tangogitlab@gmail.com>
2025-05-19 21:13:03 -0300

    services for delete and update a task has been created

--- SRC/services/taskService.ts
+++ SRC/services/taskService.ts
@@ -1,5 +1,6 @@
 import taskModel from "../models/TaskModel.js";
 import { Types } from "mongoose";
+import ITask from "../models/taskInterface.js";
 
 export default new class taskService {
     
@@ -11,9 +12,17 @@ export default new class taskService {
         }));
     }
 
-    async createTask(text : string) : Promise<object> {
-        const newTask = await taskModel.create({ text : text});
+    async createTask(text : string) : Promise<ITask> {
+        return await taskModel.create({ text : text});
+    }
 
-        return newTask;
+    async updateTask(task : ITask) : Promise<ITask | null> {
+        const { _id, text } = task;
+
+        return await taskModel.findByIdAndUpdate(_id, {text : text}, { new: true});
+    }
+
+    async deleteTask(_id : string) : Promise<void> {
+        await taskModel.findByIdAndDelete(_id);
     }
 }
