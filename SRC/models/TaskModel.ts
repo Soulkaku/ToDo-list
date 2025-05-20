32c695540f38ffcf04c1f7c0f72f479f02c6d6ee
Soulkaku <tangogitlab@gmail.com>
2025-05-19 21:14:06 -0300

    no changes

--- SRC/models/TaskModel.ts
+++ SRC/models/TaskModel.ts
@@ -1,14 +1,8 @@
 import mongoose ,{ Types, Schema, version } from "mongoose";
 import ITask from "./taskInterface.js";
 
-// interface ITask {
-//     _id : Types.ObjectId;
-//     text: string;
-// }
-
 const taskSchema = new Schema<ITask>({
     text: { type: String, required: true}
 }, { versionKey: false });
 
-export default mongoose.model('Tasks', taskSchema);
-// export default mongoose.model<ITask>('Tasks', taskSchema);
+export default mongoose.model<ITask>('Tasks', taskSchema);
